2218827609a26ec03e8d935e6414fcfed0333b1a
user-tzw <869233763@qq.com>
2018-11-01 15:10:25 +0800

    更新eureka注释

--- springCloudService/src/main/java/com/example/springCloudService/controller/HelloController.java
+++ springCloudService/src/main/java/com/example/springCloudService/controller/HelloController.java
@@ -1,5 +1,8 @@
 package com.example.springCloudService.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -7,4 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class HelloController {
+    @Value("${server.port}")
+    private int serverPort;
+
+    @RequestMapping("/print")
+    public String serverPortPrint(@RequestParam(value = "name",defaultValue = "oscar") String name){
+
+        return "Hi"+ name +" ,i am from port："+serverPort;
+    }
 }
